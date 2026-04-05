75805f9153aa5bd3b52aed6aa6776860c4525d6e
Grumpei <vallum@gmx.net>
2026-04-05 11:25:08 +0200

    Reformat Service.cs for consistency and clarity

--- ffxiv2Mqtt/Service.cs
+++ ffxiv2Mqtt/Service.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
@@ -10,22 +10,24 @@ namespace Ffxiv2Mqtt;
 [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
 public class Service
 {
-    internal static               PlayerEvents           PlayerEvents;
-    internal static               MqttManager            MqttManager;
-    [PluginService] public static IDalamudPluginInterface PluginInterface     { get; private set; }
-    [PluginService] public static IChatGui               ChatGui             { get; private set; }
-    [PluginService] public static IToastGui              ToastGui            { get; private set; }
-    [PluginService] public static IClientState           ClientState         { get; private set; }
-    [PluginService] public static IPartyList             PartyList           { get; private set; }
-    [PluginService] public static ICommandManager        CommandManager      { get; private set; }
-    [PluginService] public static IDataManager           DataManager         { get; private set; }
-    [PluginService] public static IFramework             Framework           { get; private set; }
-    [PluginService] public static IObjectTable           ObjectTable         { get; private set; }
-    [PluginService] public static IGameGui               GameGui             { get; private set; }
-    [PluginService] public static IDutyState             DutyState           { get; private set; }
-    [PluginService] public static IJobGauges             JobGauges           { get; private set; }
-    [PluginService] public static ICondition             Condition           { get; private set; }
-    [PluginService] public static INotificationManager   NotificationManager { get; private set; }
-    [PluginService] public static IPluginLog             Log                 { get; private set; }
+    internal static PlayerEvents PlayerEvents;
+    internal static MqttManager MqttManager;
+
+    [PluginService] public static IDalamudPluginInterface PluginInterface { get; private set; }
+    [PluginService] public static IChatGui ChatGui { get; private set; }
+    [PluginService] public static IToastGui ToastGui { get; private set; }
+    [PluginService] public static IClientState ClientState { get; private set; }
+    [PluginService] public static IPartyList PartyList { get; private set; }
+    [PluginService] public static ICommandManager CommandManager { get; private set; }
+    [PluginService] public static IDataManager DataManager { get; private set; }
+    [PluginService] public static IFramework Framework { get; private set; }
+    [PluginService] public static IObjectTable ObjectTable { get; private set; }
+    [PluginService] public static IGameGui GameGui { get; private set; }
+    [PluginService] public static IDutyState DutyState { get; private set; }
+    [PluginService] public static IJobGauges JobGauges { get; private set; }
+    [PluginService] public static ICondition Condition { get; private set; }
+    [PluginService] public static INotificationManager NotificationManager { get; private set; }
+    [PluginService] public static IPluginLog Log { get; private set; }
+    [PluginService] public static IAddonLifecycle AddonLifecycle { get; private set; }
 }
 #pragma warning restore CS8618
